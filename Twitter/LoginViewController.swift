5cdfcc307cdd9baf79b4d6c802bed2aebfccf2d0
Steven Tran <steventraaan@gmail.com>
2019-02-26 19:04:54 -0800

    created twitter login screen

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Steven Tran on 2/26/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        
+        let myURL = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myURL, success: {
+            // successful login
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
+        }, failure: { (Error) in
+            print("Could Not Log In!")
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
